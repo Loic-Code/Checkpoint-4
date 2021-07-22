6a5c2750fd40ec83890c40ae53906ac4e7d50d8d
Loic-Code <loic.pinguet.code@gmail.com>
2021-07-22 16:59:54 +0200

    second commit

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -31,6 +31,8 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        throw new \LogicException();
+
+        return $this->render('home/index.html.twig');
     }
 }
